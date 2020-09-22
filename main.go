afeea7fe95562471c93382aa88fca72ecd3ab611
kzkick2nd <aruga.kazuki@gmail.com>
2020-09-23 00:06:50 +0900

    run sha256

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+    "crypto/sha256"
+    "encoding/hex"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("init")
+	h := sha256.Sum256([]byte("Password"))
+	fmt.Println(hex.EncodeToString(h[:]))
 }
