b390b43593f99ae53338ae43786be49c6976ed68
kzkick2nd <aruga.kazuki@gmail.com>
2020-09-23 15:28:34 +0900

    run aes gcm

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"encoding/hex"
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
+
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 )
 
 func main() {
@@ -18,4 +22,48 @@ func main() {
 
 	hash, _ := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
 	fmt.Println(string(hash))
+
+	key, _ := hex.DecodeString("6368616e676520746869732070617373776f726420746f206120736563726574")
+    cipherText, _ := encryptByGCM(key, "12345")
+	fmt.Println(hex.EncodeToString(cipherText))
+    decryptedText, _ := decryptByGCM(key, cipherText)
+    fmt.Printf("Decrypted Text: %v\n ", decryptedText)
+        // Decrypted Text: 12345
+
+}
+
+func encryptByGCM(key []byte, plainText string) ([]byte, error) {
+    block, err := aes.NewCipher(key); if err != nil {
+        return nil, err
+    }
+
+    gcm, err := cipher.NewGCM(block); if err != nil {
+        return nil, err
+    }
+
+    nonce := make([]byte, gcm.NonceSize())// Unique nonce is required(NonceSize 12byte)
+    _, err = rand.Read(nonce); if err != nil {
+        return nil, err
+    }
+
+    cipherText := gcm.Seal(nil, nonce, []byte(plainText), nil)
+    cipherText = append(nonce, cipherText...)
+
+    return cipherText, nil
+}
+
+func decryptByGCM(key []byte, cipherText []byte) (string, error) {
+    block, err := aes.NewCipher(key); if err != nil {
+        return "", err
+    }
+    gcm, err := cipher.NewGCM(block); if err != nil {
+        return "", err
+    }
+
+    nonce := cipherText[:gcm.NonceSize()]
+    plainByte, err := gcm.Open(nil, nonce, cipherText[gcm.NonceSize():], nil); if err != nil {
+        return "", err
+    }
+
+    return string(plainByte), nil
 }
